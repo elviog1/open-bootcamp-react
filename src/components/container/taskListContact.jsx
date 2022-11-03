f44383a3eda532f7efb60d954068fe6bcdc79d37
Elvio Galeano <elvio.galeano827@gmail.com>
2022-11-03 17:05:35 -0300

    fix

--- src/components/container/taskListContact.jsx
+++ src/components/container/taskListContact.jsx
@@ -2,8 +2,14 @@ import React from 'react'
 import TaskContacto from '../pure/taskContacto'
 import { Contacto } from '../../models/Contacto'
 
-const TaskListContact = props => {
+const TaskListContact = () => {
     const defaultContact = new Contacto("Elvio","Galeano","elvio.galeano827@gmail.com",false)
+    // const defaultContact = {
+    //   Nombre: "Elvito",
+    //   Apellido: "Galeano",
+    //   Email: "asddsa",
+    //   Conectado: false
+    // }
   return (
     <div>
         <h1>Your task:</h1>
--- src/components/pure/taskContacto.jsx
+++ src/components/pure/taskContacto.jsx
@@ -1,14 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import { Contacto } from '../../models/Contacto'
 
 const TaskContacto = ({contact}) => {
+  const [conectado,setConectado] = useState(contact.Conectado)
+  console.log(conectado)
   return (
     <div>
         <h2>Nombre: {contact.Nombre}</h2>
         <h2>Apellido: {contact.Apellido}</h2>
         <h2>Email: {contact.Email}</h2>
-        <h2>Conectado: {contact.Conectado ? "Contacto en Linea" : "Contacto No Disponible"}</h2>
+        <h2> {!conectado ? "Contacto en Linea" : "Contacto No Disponible"}</h2>
+        <button onClick={()=> setConectado(!conectado)}>{conectado ? "Conectar" : "Desconectar"}</button>
     </div>
   )
 }
